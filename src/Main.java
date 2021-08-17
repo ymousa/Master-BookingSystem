77b2229fa5e8e0c5f0edf12d28fac2888c249425
ymousa <youhanna.mousa@telekom.de>
2021-08-17 14:28:00 +0200

    working with setOnCloseRequeset add a logOut method for saving data after closing the window

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -17,6 +21,31 @@ public class Main extends Application {
         scene.getStylesheets().add("Stylesheets/Login.css");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        primaryStage.setOnCloseRequest(event -> {
+            event.consume();
+            try {
+                logOut(primaryStage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public void logOut(Stage stage) throws IOException {
+        Alert alert= new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("LogOut");
+        alert.setHeaderText("Alle Daten werden nach dem Ende des Programmes gespeichert!");
+        alert.setContentText("Möchten Sie die Anwendung schließen?");
+        if(alert.showAndWait().get()== ButtonType.OK){
+            System.out.println("You successfully logged out!");
+            MainController ctrl= MainController.getInstance();
+
+            ctrl.getMyFileHelper().writeUsers(ctrl.getHmUsers());
+            ctrl.getMyFileHelper().writeBookings(ctrl.getLsBookings());
+
+            stage.close();
+        }
     }
 
     public static void main(String[] args) {
